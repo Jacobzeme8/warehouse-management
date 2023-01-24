486ce5aeb615dd3bd26e54a29860712572b0bfd0
Jacob <Jacobzeme8@gmail.com>
2023-01-24 16:48:43 -0700

    some condensed functions

--- app.js
+++ app.js
@@ -111,16 +111,16 @@ function drawPackages(array){
           <h3>Weight: ${package.weight}</h3>
           <h3>Priority Level: ${package.priorityLevel}</h3>
         </div>`)
-      packageRow.innerHTML = packageRowTemplate
-  // if(packages = undefined){
-  //   packageRowTemplate = `
-  //   <div class="col-12 border-green">
-  //     <h1>There are no packages with the current filters</h1>
-  //     <h3>please reset the filters and try again</h3>
-  //   </div>
-  //   `
-  //   packageRow.innerHTML = packageRowTemplate
-  // }
+  packageRow.innerHTML = packageRowTemplate
+  if(packages.length == 0){
+    packageRowTemplate = `
+    <div class="col-12 border-green">
+      <h1>There are no packages with the current filters</h1>
+      <h3>please reset the filters and try again</h3>
+    </div>
+    `
+    packageRow.innerHTML = packageRowTemplate
+  }
 }
 drawPackages(packages)
 
@@ -131,54 +131,38 @@ function resetPackages(){
 
 //  SECTION fragile filters
 
-function filterByFragile(){
-  let fragilePackages = packages.filter(package => package.isFragile)
-  packages = fragilePackages
-  drawPackages(packages)
-}
 
-function filterByNonFragile(){
-  let nonFragile = packages.filter(package => !package.isFragile)
-  packages = nonFragile
+function filterFragile(bool){
+  let testFragile = packages.filter(package => package.isFragile == bool)
+  packages = testFragile
   drawPackages(packages)
 }
 
 // SECTION priority filters
 
-function filterByFree(){
-  let freePriority = packages.filter(package => package.priorityLevel == "free")
-  packages = freePriority
+function filterByPriority(priority){
+  let packagePriority = packages.filter(package => package.priorityLevel == priority)
+  packages = packagePriority
   drawPackages(packages)
 }
 
-function filterByStandard(){
-  let standardPriority = packages.filter(package => package.priorityLevel == "standard")
-  packages = standardPriority
-  drawPackages(packages)
-}
-
-function filterByPriority(){
-  let expressPriority = packages.filter(package => package.priorityLevel == "express")
-  packages = expressPriority
-  drawPackages(packages)
-}
 
 // SECTION weight filter
 
-function filterByLowWeight(){
-  let lowWeight = packages.filter(package => package.weight < 1)
-  packages = lowWeight
+function filterWeight(weightOne, weightTwo){
+  if(weightTwo == 0 && weightOne ==1){
+   let weight = packages.filter(package => package.weight < weightOne)
+   packages = weight
   drawPackages(packages)
-}
-
-function filterByMidWeight(){
-  let midWeight = packages.filter(package => package.weight >= 1 && package.weight <= 10)
-  packages = midWeight
-  drawPackages(packages)
-}
-
-function filterByHighWeight(){
-  let highWeight = packages.filter(package => package.weight > 10)
-  packages = highWeight
-  drawPackages(packages)
+  }
+  else if(weightTwo == 0){
+    let weight = packages.filter(package => package.weight > weightOne)
+    packages = weight
+    drawPackages(packages)
+  }
+  else{
+    let weight = packages.filter(package => package.weight >= weightOne && package.weight <= weightTwo)
+    packages = weight
+    drawPackages(packages)
+  }
 }
